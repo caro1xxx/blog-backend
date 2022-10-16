446a055223e79b3c44860fc3706da979d150e023
wakeup <caro1xxxhv@gmail.com>
2022-10-16 20:47:01 +0800

    add PostDetail get method

--- blogbackend/views.py
+++ blogbackend/views.py
@@ -14,7 +14,7 @@ class PostList(APIView):
     def get(self,request,*args,**kwargs):
         ret = {'code':200,'msg':'ok'}
         try:
-            ret['post'] = serializers.serialize("json", Post.objects.all())
+            ret['post'] = serializers.serialize("json", Post.objects.all().order_by('-update_time'))
             return JsonResponse(ret)
             # return Response(ret)
         except Exception as e:
@@ -34,7 +34,21 @@ class PostDetail(APIView):
                 return JsonResponse(ret)
             ret['post'] = serializers.serialize("json", Post.objects.filter(post_id=id))
             return JsonResponse(ret)
-            # return Response(ret)
+        except Exception as e:
+            ret['code'] = 500
+            ret['msg'] = 'Timeout'
+        return JsonResponse(ret)
+
+    def get(self,request,*args,**kwargs):
+        ret = {'code':200,'msg':'ok'}
+        try:
+            searchkey = request.GET.get('SearchKey')
+            if not id:
+                ret['code'] = 404
+                ret['msg'] = 'Post does not exist'
+                return JsonResponse(ret)
+            ret['post'] = serializers.serialize("json", Post.objects.filter(title__icontains=searchkey).order_by('-update_time'))
+            return JsonResponse(ret)
         except Exception as e:
             ret['code'] = 500
             ret['msg'] = 'Timeout'
